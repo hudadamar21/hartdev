7f0dcc97c48ecef8b87432798af974e0f922a23c
Huda Damar <hudadamar21@gmail.com>
2021-09-10 07:56:48 +0700

    fix meta image 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,9 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-react-helmet`
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: config.GOOGLE_ANALYTIC_TRACKING_ID,
@@ -195,24 +198,19 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `HartDev`,
-        short_name: `Hartdev`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-react-helmet`
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `HartDev`,
+    //     short_name: `Hartdev`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/logo.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+    // `gatsby-plugin-gatsby-cloud`,
   ],
 }
