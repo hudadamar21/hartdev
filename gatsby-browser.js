37dbbddac33d975fe4db95f12748500efa0e1f73
Huda Damar <hudadamar21@gmail.com>
2021-09-15 18:41:04 +0700

    quick change darkmode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,7 @@ import './src/styles/tailwind.css';
 
 // Highlighting for code blocks
 import "./src/styles/prism-theme-monokai.css"
+
+const isDarkmode = JSON.parse(window.localStorage.getItem('darkmode'))
+if(isDarkmode) window.document.body.classList.add('dark') 
+else window.document.body.classList.remove('dark')
