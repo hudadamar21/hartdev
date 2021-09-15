90a572081144621edbf20c628112289ebb5403e7
Huda Damar <hudadamar21@gmail.com>
2021-09-15 21:01:30 +0700

    remove google ads

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,7 @@
 const React = require('react')
 
 const HeadComponents = [
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1247746409733600" crossOrigin="anonymous"></script>,
-  <script async="async" data-cfasync="false" src="//hungrylongingtile.com/c6cb249243f68f49699f7911e0405f8d/invoke.js"></script>,
-  
+  <script async="async" data-cfasync="false" src="//hungrylongingtile.com/c6cb249243f68f49699f7911e0405f8d/invoke.js"></script>
 ]
 
 exports.onRenderBody = ({ setHeadComponents }) => {
